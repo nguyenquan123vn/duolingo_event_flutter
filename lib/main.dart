f770fcb6bb4264540a414f19f6e3ee48d4f5904f
nguyenquan123vn <nguyenquan1005vn@gmail.com>
2021-03-11 13:32:13 +0700

    add_screenshot

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import './screens/login/signup.dart';
+import 'package:duolingo_event_app/screens/login/signup.dart';
+import 'package:duolingo_event_app/screens/login/logIn.dart';
+import 'package:duolingo_event_app/screens/event_details/event_detail.dart';
+import 'package:duolingo_event_app/screens/event_home/event_home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      home: SignUp(),
+      home: EventsScreen(),
     );
   }
 }
