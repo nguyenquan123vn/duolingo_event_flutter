8a7d77bbfbcaf1137f52ee8c3368f0d1b45f8cba
huypham99 <huyminnx99@gmail.com>
2021-03-11 11:36:19 +0700

    Add global style

---
+++ lib/global/style.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+const Color defaultBackgroundColor = Color.fromRGBO(255, 255, 255, 1);
+
+const Color defaultBrandColor = Color.fromRGBO(28, 176, 246, 1);
+
+const Color defaultTextColor = Color.fromRGBO(56, 64, 71, 1);
+
+const Color washTextColor = Color.fromRGBO(124, 136, 148, 1);
+
+const Color borderColor = Color.fromRGBO(235, 236, 237, 1);
+
+const Color iconColor = Color.fromRGBO(124,136,148,0.78);
+
+const TextStyle defaultTextStyle =
+    TextStyle(fontFamily: 'Roboto', fontSize: 13, color: defaultTextColor);
+
+const TextStyle defaultBoldWashTextStyle = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 13,
+    color: washTextColor,
+    fontWeight: FontWeight.bold);
+
+const TextStyle defaultBoldTextStyle = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 13,
+    color: defaultTextColor,
+    fontWeight: FontWeight.bold);
+
+const TextStyle headerTextStyle = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 18,
+    color: defaultTextColor,
+    fontWeight: FontWeight.bold);
+
+BoxShadow defaultShadow = BoxShadow(
+  color: Color.fromRGBO(235, 236, 237, 1),
+  spreadRadius: 0,
+  blurRadius: 0,
+  offset: Offset(0, 2),
+);
