f30726b484e732cdd16f582a27a87f9dec2d0710
huypham99 <huyminnx99@gmail.com>
2021-03-12 23:31:59 +0700

    Add new text styles and colors

--- lib/global/style.dart
+++ lib/global/style.dart
@@ -6,6 +6,8 @@ const Color defaultBrandColor = Color.fromRGBO(28, 176, 246, 1);
 
 const Color defaultTextColor = Color.fromRGBO(56, 64, 71, 1);
 
+const Color reverseTextColor = Color.fromRGBO(255, 255, 255, 1);
+
 const Color washTextColor = Color.fromRGBO(124, 136, 148, 1); // Can use for input label
 
 const Color borderColor = Color.fromRGBO(235, 236, 237, 1);
@@ -13,7 +15,10 @@ const Color borderColor = Color.fromRGBO(235, 236, 237, 1);
 const Color iconColor = Color.fromRGBO(124,136,148,0.78);
 
 const TextStyle defaultTextStyle =
-    TextStyle(fontFamily: 'Roboto', fontSize: 13, color: defaultTextColor);
+    TextStyle(fontFamily: 'Roboto', fontSize: 13, color: defaultTextColor, height: 2);
+
+const TextStyle appBarTextStyle =
+    TextStyle(fontFamily: 'Monsserat', fontSize: 24, color: reverseTextColor);
 
 const TextStyle defaultBoldWashTextStyle = TextStyle(
     fontFamily: 'Roboto',
@@ -27,9 +32,22 @@ const TextStyle defaultBoldTextStyle = TextStyle(
     color: defaultTextColor,
     fontWeight: FontWeight.bold);
 
+const TextStyle primaryBtnTextStyle = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 14,
+    color: reverseTextColor,
+    fontWeight: FontWeight.bold);
+
+const TextStyle whiteBtnTextStyle = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 14,
+    color: defaultBrandColor,
+    fontWeight: FontWeight.bold);
+
+
 const TextStyle headerTextStyle = TextStyle(
     fontFamily: 'Roboto',
-    fontSize: 18,
+    fontSize: 20,
     color: defaultTextColor,
     fontWeight: FontWeight.bold);
 
