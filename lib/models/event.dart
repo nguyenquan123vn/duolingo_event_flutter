8c1f7abcbd766925cff275faa862320a84ab44e4
huypham99 <huyminnx99@gmail.com>
2021-03-15 01:44:36 +0700

    Create event model

---
+++ lib/models/event.dart
@@ -0,0 +1,28 @@
+class Event {
+  final int attendeeLimit;
+  final String attendeeProficiency;
+  final String description;
+  final int duration;
+  final String eventId;
+  final int hostId;
+  final String language;
+  final String recurrencePattern;
+  final int reservationCount;
+  final String startDate;
+  final String timeZone;
+  final String title;
+
+  Event({
+      this.attendeeLimit,
+      this.attendeeProficiency,
+      this.description,
+      this.duration,
+      this.eventId,
+      this.hostId,
+      this.language,
+      this.recurrencePattern,
+      this.reservationCount,
+      this.startDate,
+      this.timeZone,
+      this.title});
+}
