91bdc7b498e20751a6c93c15764e14d7016e88b9
huypham99 <huyminnx99@gmail.com>
2021-03-16 21:35:00 +0700

    Adding provider to widget tree

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
+import 'package:duolingo_event_app/provider/eventFilter.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'route_generator.dart';
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Duolingo Events Clone',
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/detail',
-      onGenerateRoute: RouteGenerator.onGenerateRoute
+    return MultiProvider(
+      providers: [
+        Provider<EventFilter>(create: (_) => EventFilter())
+      ],
+      child: MaterialApp(
+          title: 'Duolingo Events Clone',
+          debugShowCheckedModeBanner: false,
+          initialRoute: '/',
+          onGenerateRoute: RouteGenerator.onGenerateRoute),
     );
   }
 }
