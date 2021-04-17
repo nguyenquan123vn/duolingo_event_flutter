941d5a2bcc18bac10e13826d919c2abb23020bf7
nguyenquan123vn <nguyenquan1005vn@gmail.com>
2021-04-17 19:53:46 +0700

    flutter_web_config

---
+++ test/mock.dart
@@ -0,0 +1,9 @@
+import 'package:duolingo_event_app/service/authentication/authentication_service.dart';
+import 'package:flutter/material.dart';
+import 'package:mockito/mockito.dart';
+
+class MockAuthService extends Mock implements AuthService {}
+
+class MockWidgetsBinding extends Mock implements WidgetsBinding {}
+
+class MockNavigatorObserver extends Mock implements NavigatorObserver {}
