2fec754fe0f35e164b9109f9abd230273ca7647f
hoang031099 <hoang031099@gmail.com>
2021-04-21 23:42:01 +0700

    tests

--- test/dropdown_filter_test.dart
+++ test/dropdown_filter_test.dart
@@ -1,4 +1,4 @@
-import 'package:duolingo_event_app]provider/eventFilter.dart";
+import 'package:duolingo_event_app]provider/eventFilter.dart';
 import 'package:duolingo_event_app/screens/event_home/components/dropdownBar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
